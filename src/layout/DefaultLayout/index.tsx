380f06c1c43a28c64048878f4cf59d4834b35714
brenoq <brenoq@gmail.com>
2023-06-09 16:36:07 -0300

    Countdown functionality

--- src/layout/DefaultLayout/index.tsx
+++ src/layout/DefaultLayout/index.tsx
@@ -1,7 +1,7 @@
 import { Outlet } from 'react-router-dom'
 import { LayoutContainer } from './styles'
 
-import { Header } from '../../components/Header/index'
+import { Header } from '../../components/Header'
 
 export function DefaultLayout() {
   return (
